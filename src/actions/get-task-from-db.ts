6f7dc5610052b01b6a7157ff165652a539eefae2
Pedro vital <pedro.machadoo@hotmail.com>
2026-08-11 09:58:29 -0300

    feat: corrigir função getTasks para buscar tarefas do banco de dados

--- src/actions/get-task-from-db.ts
+++ src/actions/get-task-from-db.ts
@@ -1,5 +1,10 @@
+"use server"
 import {prisma} from "@/utils/prisma"
 
-const getTasks = async () => {
-const task = await  prisma.task.findMany
+export const getTasks = async () => {
+const tasks= await  prisma.task.findMany()
+
+if (!tasks) return 
+console.log (tasks) 
+return tasks
 }
